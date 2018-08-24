291a2c193f2c77dba7d99c163e330352429fe01f
safordog <40581256+safordog@users.noreply.github.com>
2018-08-24 20:35:24 +0300

    Update and rename Main.java to Task 3 - Main.java

--- Task 3 - Main.java
+++ Task 3 - Main.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Напишите программу, которая выведет в файл все ссылки,
+ * которые содержатся в html документе, который будет прислан в
+ * результате запроса на произвольный URL.
  */
 package homework11b;
 
